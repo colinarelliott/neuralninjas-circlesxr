7ef197fc858323a41997a86dfaa4f0663d91190a
Dylan <68760594+Zoykz@users.noreply.github.com>
2023-03-12 15:47:57 -0400

    Teleport stuff

---
+++ src/worlds/AI_Museum/scripts/teleport.js
@@ -0,0 +1,84 @@
+'use strict';
+const ascene = document.querySelector('a-scene');
+
+const buttonLeftPast = document.querySelector('#buttonLeftPast');
+const buttonRightPast = document.querySelector('#buttonRightPast');
+const buttonLeftPresent = document.querySelector('#buttonLeftPresent');
+const buttonRightPresent = document.querySelector('#buttonRightPresent');
+const buttonLeftFuture = document.querySelector('#buttonLeftFuture');
+const buttonRightFuture = document.querySelector('#buttonRightFuture');
+
+const player = document.querySelector('#player');
+
+let id = "";
+
+
+AFRAME.registerComponent('experience-manager', {
+    init: function () {
+        const socket = io();
+
+        socket.on('connect', (userData) => {
+
+            buttonLeftPast.addEventListener('click', function () {
+                socket.emit('buttonLeftPast-selected', { id: id });
+            });
+            buttonRightPast.addEventListener('click', function () {
+                socket.emit('buttonRightPast-selected', { id: id });
+            });
+            buttonLeftPresent.addEventListener('click', function () {
+                socket.emit('buttonLeftPresent-selected', { id: id });
+            });
+            buttonRightPresent.addEventListener('click', function () {
+                socket.emit('buttonRightPresent-selected', { id: id });
+            });
+            buttonLeftFuture.addEventListener('click', function () {
+                socket.emit('buttonLeftFuture-selected', { id: id });
+            });
+            buttonRightFuture.addEventListener('click', function () {
+                socket.emit('buttonRightFuture-selected', { id: id });
+            });
+
+        });
+
+
+        socket.on('setup', (data) => {
+            id = data.id;
+        });
+
+        socket.on('teleport', (data) => {
+            if (id == data.id) {
+                setTimeout(
+                    function () {
+                        let pos = player.getAttribute('position');
+                        player.setAttribute('position', `${pos.x + data.x} ${pos.y} ${pos.z}`);
+                    }, 5000);
+            }
+        });
+
+        socket.on('button-click', (data) => {
+
+            let loc = document.querySelector(`#${data.button}`);
+
+            loc.setAttribute('animation-mixer', 'clip: *; loop: once; clampWhenFinished: true;');
+            loc.addEventListener('animation-finished', function () {
+                loc.removeAttribute('animation-mixer');
+            }, { once: true });
+            document.querySelector(`#${data.button}Sound`).components.sound.playSound();
+
+        });
+
+
+        socket.on('initiate-teleport', (data) => {
+            console.log("In initiate teleport");
+            let capsuleCurrent = document.querySelector(`#${data.current}`);
+
+            capsuleCurrent.setAttribute('animation-mixer', 'clip: *; loop: once; clampWhenFinished: true;');
+            capsuleCurrent.addEventListener('animation-finished', function () {
+                capsuleCurrent.removeAttribute('animation-mixer');
+                socket.emit('enable-teleportation');
+            }, { once: true });
+
+        });
+    }
+})
+
