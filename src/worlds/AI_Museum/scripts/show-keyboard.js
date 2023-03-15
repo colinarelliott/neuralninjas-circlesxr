1bf4226004a163e92fef4666c7bd799b49b098a9
Colin Elliott <colinarelliott@gmail.com>
2023-03-15 00:45:24 -0400

    attempting to fix keyboard disappearing bug

--- src/worlds/AI_Museum/scripts/show-keyboard.js
+++ src/worlds/AI_Museum/scripts/show-keyboard.js
@@ -7,17 +7,27 @@ AFRAME.registerComponent("set-keyboard", {
     const CONTEXT = this;
     //Get the elements we need
     const keyboard = document.querySelector("#keyboard");
+    const keyboardComponent = document.querySelector("#keyboard").components["super-keyboard"];
     const imageGenerator = document.querySelector("[open-ai-image-gen]").components['open-ai-image-gen'];
+    const scene = document.querySelector("a-scene");
 
-    console.log(keyboard.childNodes);
+    keyboard.addEventListener("superkeyboardchange", () => {
+      //the text changed, update the big prompt text?
+    });
 
     //When the user presses enter on the keyboard, generate an image
-    keyboard.addEventListener("superkeyboardinput", () => {
+    keyboard.addEventListener("superkeyboardinput", (data) => {
+      //ask open-ai-image-gen to generate an image
       imageGenerator.generateImage({
         prompt: CONTEXT.data.keyboardValue,
         n: 1,
         size: "1024x1024",
       });
+      //IDK HOW TO GET THE KEYBOARD TO STOP DISSAPEARING! I'M GONNA GO TO BED.
+      keyboardComponent.data.value = "";
+      CONTEXT.data.keyboardValue = "";
+      keyboardComponent.data.show = true;
+      keyboard.setAttribute("visible", true);
     });
   },
 
