eeb5fac11cece938e3f97b0235d1a6d59a5fd56d
Colin Elliott <colinarelliott@gmail.com>
2023-03-14 23:40:25 -0400

    grab keyboardcomponent value instead of elem value

--- src/worlds/AI_Museum/scripts/show-keyboard.js
+++ src/worlds/AI_Museum/scripts/show-keyboard.js
@@ -22,7 +22,9 @@ AFRAME.registerComponent("set-keyboard", {
   },
 
   tick: function () {
-    const CONTEXT = this;
-    CONTEXT.keyboardValue = keyboard.getAttribute("value");
+    const CONTEXT = this; 
+    const keyboardComponent = document.querySelector("#keyboard").components["super-keyboard"];
+    CONTEXT.data.keyboardValue = keyboardComponent.data.value;
+    console.log(CONTEXT.data.keyboardValue);
   },
 });
