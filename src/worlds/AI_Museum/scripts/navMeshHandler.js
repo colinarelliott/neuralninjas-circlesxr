3f01510520de62b1db690a4df9e3006a39d1f236
Colin Elliott <colinarelliott@gmail.com>
2023-03-29 09:36:02 -0400

    swapping working for navmesh, not updating

--- src/worlds/AI_Museum/scripts/navMeshHandler.js
+++ src/worlds/AI_Museum/scripts/navMeshHandler.js
@@ -14,13 +14,13 @@ AFRAME.registerComponent('nav-mesh-handler', {
     init: function () {
         const THIS = this;
 
+        //bind the createNavMesh function to THIS
+        THIS.createNavMesh = THIS.createNavMesh.bind(THIS);
+
         //get the navmesh entity
         THIS.navMesh = document.querySelector('#navmesh');
         //set the properties of the navmesh to the present museum
-        THIS.navMesh.setAttribute('scale', '0.8 0.8 0.8');
-        THIS.navMesh.setAttribute('position', '0 0 0')
-        THIS.navMesh.setAttribute('gltf-model', '#present-nav-mesh');
-        THIS.navMesh.setAttribute('visible', 'true');
+
         //get the player entity
         THIS.player = document.querySelector('#Player1');
     },
@@ -32,25 +32,37 @@ AFRAME.registerComponent('nav-mesh-handler', {
         THIS.playerPos = THIS.player.getAttribute('position');
 
         console.log(THIS.playerPos.x);
-
+        
         if (THIS.playerPos.x < -25) {
             //set the properties of the navmesh to the past museum
-            THIS.navMesh.setAttribute('scale', '0.6 0.6 0.6');
-            THIS.navMesh.setAttribute('position', '-50 0 0')
-            THIS.navMesh.setAttribute('gltf-model', '#past-nav-mesh');
+            THIS.navMesh.parentNode.removeChild(THIS.navMesh);
+            THIS.createNavMesh('-50 0.02 0', '0.6 0.6 0.6', '#past-nav-mesh');
             console.log("PAST NAVMESH LOADED");
         } else if (THIS.playerPos.x > -25 && THIS.playerPos.x < 25) {
             //set the properties of the navmesh to the present museum
-            THIS.navMesh.setAttribute('scale', '0.8 0.8 0.8');
-            THIS.navMesh.setAttribute('position', '0 0 0')
-            THIS.navMesh.setAttribute('gltf-model', '#present-nav-mesh');
+            THIS.navMesh.parentNode.removeChild(THIS.navMesh);
+            THIS.createNavMesh('0 0.02 0', '0.8 0.8 0.8', '#present-nav-mesh');
             console.log("PRESENT NAVMESH LOADED");
         } else if (THIS.playerPos.x > 25) {
             //set the properties of the navmesh to the future museum
-            THIS.navMesh.setAttribute('scale', '1 1 1');
-            THIS.navMesh.setAttribute('position', '50 0 0')
-            THIS.navMesh.setAttribute('gltf-model', '#future-nav-mesh');
+            THIS.navMesh.parentNode.removeChild(THIS.navMesh);
+            THIS.createNavMesh('50 0.02 0', '1 1 1', '#future-nav-mesh');
             console.log("FUTURE NAVMESH LOADED");
         }
-    }
+    },
+
+    createNavMesh: function (position, scale, model) {
+        const THIS = this;
+
+        //create the navmesh entity
+        THIS.navMesh = document.createElement('a-entity');
+        THIS.navMesh.setAttribute('id', 'navmesh');
+        THIS.navMesh.setAttribute('scale', scale);
+        THIS.navMesh.setAttribute('position', position)
+        THIS.navMesh.setAttribute('gltf-model', model);
+        THIS.navMesh.setAttribute('visible', 'true');
+
+        //add the navmesh entity to the scene
+        THIS.el.sceneEl.appendChild(THIS.navMesh);
+    },
 });
