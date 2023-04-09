5ae2524630a097b17731dcabb5ac7263fc6b56e3
Colin Elliott <colinarelliott@gmail.com>
2023-04-09 12:06:26 -0400

    added optimized past museum model

--- src/worlds/AI_Museum/scripts/slideTimer.js
+++ src/worlds/AI_Museum/scripts/slideTimer.js
@@ -1,11 +1,8 @@
 //simple slide incrementer script
 
-
-/*
-
 AFRAME.registerComponent('slide-timer', {
     schema: {
-        slideNum: {type: 'number', default: 1},
+        slide: {type: 'number', default: 1},
         duration: {type: 'number', default: 1000},
         numberOfSlides: {type: 'number', default: 10},
     },
@@ -16,46 +13,39 @@ AFRAME.registerComponent('slide-timer', {
         THIS.incrementSlide = THIS.incrementSlide.bind(THIS);
         THIS.decrementSlide = THIS.decrementSlide.bind(THIS);
         THIS.resetSlide = THIS.resetSlide.bind(THIS);
-        
-        //throttle the tick to 1000ms
-        THIS.tick = AFRAME.utils.throttleTick(THIS.tick, THIS.data.duration);
+
+        THIS.slide = 1;
+
+        THIS.billboard = this.el;
     },
 
     tick: function () {
         const THIS = this;
 
-        console.log("slide: " + THIS.data.slideNum);
-
-        if (THIS.data.slideNum < THIS.data.numberOfSlides) { 
+        if (THIS.data.slide < THIS.data.numberOfSlides) { 
             THIS.incrementSlide(); 
         } else { 
             THIS.resetSlide();
         };
-    },
 
-    update : function () {
-        const THIS = this;
-        const billboard = document.querySelector('#UseCaseBillboard');
-
-        let copy = billboard.cloneNode(true);
-        copy.setAttribute('material', 'src: #UseCase' + THIS.data.slideNum);
-        billboard.parentNode.replaceChild(copy, billboard);
+        let copy = THIS.billboard.cloneNode(true);
+        copy.setAttribute('material', 'src: #UseCase' + THIS.data.slide);
+        THIS.billboard.parentNode.removeChild(THIS.billboard);
+        THIS.billboard = copy;
     },
 
     incrementSlide: function () {
         const THIS = this;
-        THIS.data.slideNum++;
+        THIS.data.slide++;
     },
 
     decrementSlide: function () {
         const THIS = this;
-        THIS.data.slideNum--;
+        THIS.data.slide--;
     },
 
     resetSlide: function () {
         const THIS = this;
-        THIS.data.slideNum = 1;
+        THIS.data.slide = 1;
     }
 });
-
-*/
