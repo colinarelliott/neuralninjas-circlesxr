fca66c185d4bfe26281148e7f5afaa74c0d78672
Colin Elliott <colinarelliott@gmail.com>
2023-04-09 21:13:33 -0400

    fixed use case billboard

--- src/worlds/AI_Museum/scripts/slideTimer.js
+++ src/worlds/AI_Museum/scripts/slideTimer.js
@@ -1,10 +1,13 @@
 //simple slide incrementer script
 
+//attached to the wrapper around the billboard
+
 AFRAME.registerComponent('slide-timer', {
     schema: {
-        slide: {type: 'number', default: 1},
+        currentSlide: {type: 'number', default: 1},
         duration: {type: 'number', default: 1000},
         numberOfSlides: {type: 'number', default: 10},
+        billboardID: {type: 'string', default: 'UseCaseBillboard'}
     },
 
     init: function () {
@@ -13,39 +16,74 @@ AFRAME.registerComponent('slide-timer', {
         THIS.incrementSlide = THIS.incrementSlide.bind(THIS);
         THIS.decrementSlide = THIS.decrementSlide.bind(THIS);
         THIS.resetSlide = THIS.resetSlide.bind(THIS);
+        THIS.changeSlide = THIS.changeSlide.bind(THIS);
 
-        THIS.slide = 1;
+        THIS.data.currentSlide = 1;
 
-        THIS.billboard = this.el;
+        THIS.billboardWrapper = THIS.el;
+
+        THIS.billboard = document.querySelector('#' + THIS.data.billboardID);
+
+        //setup timers
+        setInterval(THIS.incrementSlide, THIS.data.duration);
+        setTimeout( function() {
+            if (THIS.data.currentSlide < THIS.data.numberOfSlides) {
+                setInterval(THIS.changeSlide, THIS.data.duration);
+            }
+        }, 50);
     },
 
+    /* I don't understand why this function doesn't work but it keeps randomly crashing
     tick: function () {
         const THIS = this;
 
-        if (THIS.data.slide < THIS.data.numberOfSlides) { 
-            THIS.incrementSlide(); 
-        } else { 
+        THIS.billboard = document.querySelector('#UseCaseBillboard');
+
+        if (THIS.data.currentSlide > THIS.data.numberOfSlides) {
             THIS.resetSlide();
-        };
+        }
+
+        if (THIS.data.currentSlide < 1) {
+            THIS.resetSlide();
+        }
+
+        if (THIS.data.currentSlide < THIS.data.numberOfSlides) {
+            THIS.incrementSlide();
+        }
+
+        console.log("changing slide to: " + THIS.data.currentSlide);
 
         let copy = THIS.billboard.cloneNode(true);
-        copy.setAttribute('material', 'src: #UseCase' + THIS.data.slide);
+        copy.setAttribute('material', 'src: #UseCase' + THIS.data.currentSlide);
         THIS.billboard.parentNode.removeChild(THIS.billboard);
-        THIS.billboard = copy;
+        THIS.billboardWrapper.appendChild(copy);
+    }, */
+
+    changeSlide: function () {
+        const THIS = this;
+
+        let billboard = document.querySelector('#UseCaseBillboard');
+
+        if (THIS.data.currentSlide > THIS.data.numberOfSlides) { THIS.resetSlide(); }
+
+        let copy = billboard.cloneNode(true);
+        copy.setAttribute('material', 'src: #UseCase' + THIS.data.currentSlide);
+        billboard.parentNode.removeChild(billboard);
+        THIS.el.appendChild(copy);
     },
 
     incrementSlide: function () {
         const THIS = this;
-        THIS.data.slide++;
+        THIS.data.currentSlide++;
     },
 
     decrementSlide: function () {
         const THIS = this;
-        THIS.data.slide--;
+        THIS.data.currentSlide--;
     },
 
     resetSlide: function () {
         const THIS = this;
-        THIS.data.slide = 1;
+        THIS.data.currentSlide = 1;
     }
 });
